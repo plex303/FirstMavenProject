f0c93e1b28bde50dbd275e885d816adb08e9c9d7
plex303 <rmutani@gmail.com>
2020-02-18 21:38:43 +0200

    made very important changes

--- src/main/java/za/ac/cput/adp3/App.java
+++ src/main/java/za/ac/cput/adp3/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Welcome to my first maven project." );
+        System.out.println("Ron was here!!!");
     }
 }

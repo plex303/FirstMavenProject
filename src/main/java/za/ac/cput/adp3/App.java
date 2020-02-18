437c9e4172190843e754f499ce49203e50da823a
Nico <ncfortuin27@gmail.com>
2020-02-18 09:11:33 +0200

    first commit

---
+++ src/main/java/za/ac/cput/adp3/App.java
@@ -0,0 +1,13 @@
+package za.ac.cput.adp3;
+
+/**
+ * Nico Fortuin
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to my first maven project." );
+    }
+}
